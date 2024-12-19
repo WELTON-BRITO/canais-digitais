c643f4ed0547f111818bcd729156a7623620c066
welton-brito <welton.britinho@gmail.com>
2024-12-18 22:26:00 -0300

    Projeto Desafio

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,5 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
+
 export const environment = {
-  production: true,
-  url_login:  'https://multasbackendapp.herokuapp.com',
-  url_server: 'https://multasbackendapp.herokuapp.com/services',
-  url_open: 'https://multasbackendapp.herokuapp.com/open',
+  url_login:  'http://localhost:4200/',
+  url_server: 'http://localhost:4200/services',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,5 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
-
 export const environment = {
-  production: false,
-  url_login:  'https://multasbackendapp.herokuapp.com',
-  url_server: 'https://multasbackendapp.herokuapp.com/services',
-  url_open: 'https://multasbackendapp.herokuapp.com/open',
+
+  url_login:  'http://localhost:4200/',
+  url_server: 'http://localhost:4200/services',
 };
